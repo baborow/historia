05ff9e05a812a5bd919617ca740213d57bbb482b
milmaj9 <milosz.maj1234@gmail.com>
2026-01-16 00:12:32 +0100

    Add files via upload

---
+++ Mapa internetowa/fabryki_1886.js
@@ -0,0 +1,12 @@
+var fabryki_1886={
+"type": "FeatureCollection",
+"name": "fabryki_1886",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ 17.972594357546157, 50.165181116541859 ] } },
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ 17.975545617200712, 50.16467751612204 ] } },
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ 17.975549005839895, 50.156512639193217 ] } },
+
+
+]
+}
