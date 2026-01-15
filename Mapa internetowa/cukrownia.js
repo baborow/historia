ed07ead2f8684c097899c53aeb907d021fc64ac5
milmaj9 <milosz.maj1234@gmail.com>
2026-01-16 00:31:37 +0100

    Delete Mapa internetowa directory

--- Mapa internetowa/cukrownia.js
+++
@@ -1,8 +0,0 @@
-var cukrownia={
-"type": "FeatureCollection",
-"name": "cukrownia",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ 17.976579702819233, 50.157416168807046 ] } }
-]
-}
--- Mapa internetowa/fabryki_1959.js
+++
@@ -1,9 +0,0 @@
-var fabryki_1959={
-"type": "FeatureCollection",
-"name": "fabryki_1959",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ 17.972594357546157, 50.165181116541859 ] } },
-{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ 17.975549005839895, 50.156512639193217 ] } }
-]
-}
